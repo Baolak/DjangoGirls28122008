446ac39668b8b167be9d905baf9605ae9fee697b
Bartosz Kmieciak <bartosz.kmieciak@gmail.com>
2018-12-30 02:26:20 +0100

    Add static root and modify allowed host

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '79!0384p#xedqg)7$vh033@z-rx2ir^wpel5okjqz2l-=-_f!9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
